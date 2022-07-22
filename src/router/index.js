3303a454eee1639d555212d31e0e2eefd2a72939
oskar shtefan <oskar.shtefan@gmail.com>
2022-07-23 00:57:15 +0600

    added container in base.css

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Main from "../views/Main";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: "Main",
-      component: Main
-    }
-  ]
+  routes: []
 })
 
 export default router
