d6fcbd5a9f13296b65048076ce9dbd78815f7da9
oskar shtefan <oskar.shtefan@gmail.com>
2022-07-23 01:03:01 +0600

    initial commit

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <Header/>
+</template>
+
+<script>
+    import Header from "../components/Header";
+    export default {
+        name: "Main",
+        components: {Header}
+    }
+</script>
+
+<style scoped>
+
+</style>
